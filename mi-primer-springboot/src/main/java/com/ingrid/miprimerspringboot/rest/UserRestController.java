fd34e1441f23b1d4ff9418afeca152c8ce421ae7
Ingrid <ingridmendoza86@gmail.com>
2020-01-16 16:08:10 -0500

    Version inicial

---
+++ mi-primer-springboot/src/main/java/com/ingrid/miprimerspringboot/rest/UserRestController.java
@@ -0,0 +1,54 @@
+package com.ingrid.miprimerspringboot.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.ingrid.miprimerspringboot.modelo.User;
+import com.ingrid.miprimerspringboot.service.UserService;
+
+@RestController
+public class UserRestController {
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping(value = "/user/", method = RequestMethod.GET)
+	public ResponseEntity<List<User>> listAllUsers() {
+		List<User> users = userService.findAllUsers();
+		if (users.isEmpty()) {
+			return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> getUser(@PathVariable("id") long id) {
+		User user = userService.findById(id);
+		if (user == null) {
+			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/user/", method = RequestMethod.POST)
+	public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder ucBuilder) {
+		if (userService.isUserExist(user)) {
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		}
+		userService.saveUser(user);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(ucBuilder.path("/user/{id}").buildAndExpand(user.getId()).toUri());
+		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+	}
+}
