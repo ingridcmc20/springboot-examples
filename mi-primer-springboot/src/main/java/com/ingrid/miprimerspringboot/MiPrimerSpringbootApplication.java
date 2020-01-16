549d664a5d92204d39a7c667d3c756d620d37f39
unknown <ingridmendoza86@gmail.com>
2020-01-15 22:48:48 -0500

    Version inicial

---
+++ mi-primer-springboot/src/main/java/com/ingrid/miprimerspringboot/MiPrimerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ingrid.miprimerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimerSpringbootApplication.class, args);
+	}
+
+}
---
+++ mi-primer-springboot/src/test/java/com/ingrid/miprimerspringboot/MiPrimerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingrid.miprimerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
